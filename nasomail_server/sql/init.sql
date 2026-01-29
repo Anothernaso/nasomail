12255656d3edd3b5317bcdf58f78ca481f606452
Anothernaso <antondev@kuthy.com>
2026-01-29 16:18:59 +0100

    Add schema

---
+++ nasomail_server/sql/init.sql
@@ -0,0 +1,59 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS users (
+    id         INTEGER  PRIMARY KEY,
+
+    username   TEXT     UNIQUE NOT NULL COLLATE NOCASE
+        CHECK (username = TRIM(username) AND LENGTH(username) >=  3 AND LENGTH(username) <=  20),
+
+    password   TEXT     NOT NULL
+        CHECK (password = TRIM(password) AND LENGTH(password) >=  8 AND LENGTH(password) <=  20),
+
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS mails (
+    id         INTEGER  PRIMARY KEY,
+    user_id    INTEGER  NOT NULL,
+
+    subject    TEXT     NOT NULL
+        CHECK (subject = TRIM(subject) AND LENGTH(subject) <=  255),
+
+    body       TEXT     NOT NULL
+        CHECK (body = TRIM(body) AND LENGTH(body) <=  1024*1024),
+
+    sender     TEXT     NOT NULL
+        CHECK (sender = TRIM(sender) AND LENGTH(sender) <=  255),
+
+    recipient  TEXT     NOT NULL
+        CHECK (recipient = TRIM(recipient) AND LENGTH(recipient) <=  255),
+
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    status     TEXT     NOT NULL
+        CHECK (status IN ('new', 'read', 'draft', 'sent')),
+
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS attachments (
+    id         INTEGER  PRIMARY KEY,
+    mail_id    INTEGER  NOT NULL,
+
+    data       BLOB     NOT NULL
+        CHECK (length(data) <= 1024*1024*1000),
+
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (mail_id)
+        REFERENCES mails(id)
+        ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_mails_user_id
+    ON mails(user_id);
+
+CREATE INDEX IF NOT EXISTS idx_attachments_mail_id
+    ON attachments(mail_id);

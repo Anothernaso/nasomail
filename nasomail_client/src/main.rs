52cbf32b653105ffdda26d804434c0d458269501
Anothernaso <antondev@kuthy.com>
2026-02-07 18:04:21 +0100

    Add Connect command to CLI and implement server connection logic in `nasomail_client`
    Add a new Connect command that allows users to specify a server address
    to connect to. Implement the connection logic with success/failure
    handling and appropriate console output. The command parses the address,
    attempts to establish a connection, and exits with an error code if the
    connection fails.

--- nasomail_client/src/main.rs
+++ nasomail_client/src/main.rs
@@ -1,5 +1,7 @@
+use std::process::exit;
+
 use clap::Parser;
-use cli::Cli;
+use colored::Colorize;
 use tokio::task;
 
 mod auth;
@@ -7,9 +9,38 @@ mod cli;
 mod connection;
 mod meta;
 
+use cli::{Cli, Commands};
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let _cli = task::spawn_blocking(|| Cli::parse()).await?;
+    let cli = task::spawn_blocking(|| Cli::parse()).await?;
 
-    Ok(())
+    let mut exit_code = 0;
+
+    match cli.command {
+        Commands::LogIn { name, passphrase } => {}
+        Commands::LogOut => {}
+        Commands::Connect { addr } => {
+            connection::set_connection(&addr).await?;
+
+            let success = connection::test_connection().await?;
+
+            if success {
+                println!(
+                    "{}",
+                    format!("Successfully connected to server: {}", addr).green()
+                );
+            } else {
+                connection::remove_connection().await?;
+
+                exit_code = 1;
+                println!(
+                    "{}",
+                    format!("Error: Could not reach server: {}", addr).red()
+                );
+            }
+        }
+    }
+
+    exit(exit_code);
 }
